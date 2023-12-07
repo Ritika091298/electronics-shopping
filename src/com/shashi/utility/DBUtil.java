e96d05f41661c649c1c9da932ea217be6687b7a3
Ritika091298 <151695371+Ritika091298@users.noreply.github.com>
2023-12-07 13:42:50 +0000

    updates

--- src/com/shashi/utility/DBUtil.java
+++ src/com/shashi/utility/DBUtil.java
@@ -17,7 +17,7 @@ public class DBUtil {
 
 		try {
 			if (conn == null || conn.isClosed()) {
-				ResourceBundle rb = ResourceBundle.getBundle("application.properties");
+				ResourceBundle rb = ResourceBundle.getBundle("application");
 				String connectionString = rb.getString("db.connectionString");
 				String driverName = rb.getString("db.driverName");
 				String username = rb.getString("db.username");
